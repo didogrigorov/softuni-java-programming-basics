e72f2ddaf78b92892cd664af7881cd9635e7d9f3
didogrigorov <dido.grigorov@gmail.com>
2023-12-16 19:58:42 +0200

    Java Programming Basics - SoftUni

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,7 @@ public class Main {
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.print("Hello and welcome!!!\n");
 
-        for (int i = 0; i <= 10; i++) {
+        for (int i = 0; i <= 11; i++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
             System.out.println("i = " + i);
